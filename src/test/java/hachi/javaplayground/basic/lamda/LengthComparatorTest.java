394790c082d114f7c9a8e9d451a7c436b3054b2b
hachi <eojin312@naver.com>
2020-07-20 20:22:03 +0900

    생성자 레퍼런스 예제 코드

--- src/test/java/hachi/javaplayground/basic/lamda/LengthComparatorTest.java
+++ src/test/java/hachi/javaplayground/basic/lamda/LengthComparatorTest.java
@@ -2,9 +2,13 @@ package hachi.javaplayground.basic.lamda;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,5 +19,11 @@ class LengthComparatorTest {
         // 함수형 인터페이스 연습
         // BiFunction 문자열 비교 람다
         BiFunction<String, String, Integer> comp = (first, second) -> Integer.compare(first.length(), second.length());
+    }
+    @Test
+    void 생성자_레퍼런스_테스트() {
+        List<String> labels = new ArrayList<>();
+        Stream<Button> stream = labels.stream().map(Button::new);
+        List<Button> buttons = stream.collect(Collectors.toList());
     }
 }
