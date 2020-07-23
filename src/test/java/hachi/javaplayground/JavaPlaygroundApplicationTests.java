c8f0d6af6da54a19f058da08073c45e2f6aac883
hachi <eojin312@naver.com>
2020-07-23 22:06:33 +0900

    pom.xml 에 있는 기존 querydsl 은 mysema 의 query dsl 이었는데 그건 옛날버전이라 jpaQueryFactory 를 생성했을 때 문제가 생겼다. 그래서 최신 query dsl dependency 로 변경했더니 해결완료.

--- src/test/java/hachi/javaplayground/JavaPlaygroundApplicationTests.java
+++ src/test/java/hachi/javaplayground/JavaPlaygroundApplicationTests.java
@@ -1,13 +1,37 @@
 package hachi.javaplayground;
 
+
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import hachi.javaplayground.entity.Item;
+import hachi.javaplayground.entity.QItem;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 @SpringBootTest
+@Transactional
 class JavaPlaygroundApplicationTests {
+
+    @Autowired
+    private EntityManager em;
 
     @Test
     void contextLoads() {
+
+    }
+
+    @Test
+    void query_dsl_entity_잘되는지_확인() {
+        Item item = new Item();
+        em.persist(item);
+
+        JPAQueryFactory jpaQueryFactory = new JPAQueryFactory(em);
+
+        QItem qItem = new QItem("i");
     }
 
 }
