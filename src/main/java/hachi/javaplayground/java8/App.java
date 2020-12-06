f6e4adf87ae56dab91751b274ae382e4989f6cdc
hachi <eojin312@naver.com>
2020-12-06 19:51:55 +0900

    stream 공부하기

---
+++ src/main/java/hachi/javaplayground/java8/App.java
@@ -0,0 +1,34 @@
+package hachi.javaplayground.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class App {
+
+    public static List<String> show() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add("a");
+        stringList.add("b");
+        stringList.add("c");
+        stringList.add("d");
+        List<String> collect = stringList.stream()
+                .map((s) -> {
+                    System.out.println(s);
+                    return s.toUpperCase();
+                })
+                .collect(Collectors.toList());
+
+        collect.forEach(System.out::println);
+
+
+        stringList.forEach(System.out::println);
+
+        return stringList;
+    }
+
+    public static void main(String[] args) {
+        show();
+    }
+
+}
