90bd7481c0b0d826fe1f21ad77c3a783114de280
Eojin <eojin312@naver.com>
2020-12-17 21:16:19 +0900

    익명 내부 클래스 + 람다 표현식

--- src/main/java/hachi/javaplayground/java8/Foo.java
+++ src/main/java/hachi/javaplayground/java8/Foo.java
@@ -1,15 +1,36 @@
 package hachi.javaplayground.java8;
 
+import java.util.function.*;
+
 public class Foo {
     public static void main(String[] args) {
-        RunSomething runSomething = new RunSomething() {
-            int baseNumber = 10;
+        BinaryOperator<Integer> get10 = (a, b) -> a + b;
+
+        UnaryOperator<Integer> plus10 = (i) -> i + 10;
+        UnaryOperator<Integer> multiply2 = (i) -> i * 2;
+        System.out.println(plus10.andThen(multiply2).apply(2));
+    }
+
+
+    public void run() {
+        final int baseNumber = 10; // 사실상 final
+
+        // 로컬 클래스
+        class LocalClass {
+            void printBaseNumber() {
+                System.out.println(baseNumber);
+            }
+        }
+        // 익명 클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
             @Override
-            public int doIt(int number) {
-                baseNumber++;
-                return number + baseNumber;
+            public void accept(Integer integer) {
+                System.out.println(baseNumber);
             }
         };
-        System.out.println(runSomething.doIt(1));
+
+        // 람다
+        IntConsumer printInt = (i) -> System.out.println(i + baseNumber);
+        printInt.accept(10);
     }
 }
